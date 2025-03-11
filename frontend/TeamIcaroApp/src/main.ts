1370fbccdf89faa9b349bf91b8d5a0013d124533
GabReiser <gab.reiser@outlook.com>
2025-03-11 16:54:27 -0300

    ajustes na main para dar build

--- frontend/TeamIcaroApp/src/main.ts
+++ frontend/TeamIcaroApp/src/main.ts
@@ -2,10 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideAnimations } from '@angular/platform-browser/animations'; 
+import { IonicModule } from '@ionic/angular'
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -13,6 +15,7 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    importProvidersFrom(IonicModule.forRoot({})),
   ],
 });
