42e3324139633c2241d9b16aebd9a63b7f4008d7
GabReiser <gab.reiser@outlook.com>
2025-03-11 17:15:18 -0300

    ajustes icones navbar

--- frontend/TeamIcaroApp/src/main.ts
+++ frontend/TeamIcaroApp/src/main.ts
@@ -3,11 +3,25 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideAnimations } from '@angular/platform-browser/animations'; 
 import { IonicModule } from '@ionic/angular'
+import { addIcons } from 'ionicons';
+import { home, barbell, nutrition, person, restaurant, chevronDown, chevronUp } from 'ionicons/icons';
+
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+
+addIcons({
+  home,
+  barbell,
+  nutrition,
+  person,
+  restaurant,
+  'chevron-down': chevronDown,
+  'chevron-up': chevronUp
+});
+
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -17,5 +31,6 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(),
     provideAnimations(),
     importProvidersFrom(IonicModule.forRoot({})),
+    
   ],
 });
